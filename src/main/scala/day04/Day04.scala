9f7f63150078e9d577344e242e4700b65b93e123
Leonardo Nascimento dos Santos <leonardo.nascimento.dos.santos@volvocars.com>
2022-12-04 13:41:34 +0100

    day04: part 1

---
+++ src/main/scala/day04/Day04.scala
@@ -0,0 +1,42 @@
+package day04
+
+import org.apache.spark.sql.SparkSession
+import scala.io.Source
+
+object main {
+  def parseLine(line: String): ((Int,Int),(Int,Int)) = {
+    val elves = line.split(",")
+    val as = elves(0).split("-")
+    val bs = elves(1).split("-")
+    (
+      (as(0).toInt, as(1).toInt),
+      (bs(0).toInt, bs(1).toInt)
+    )
+  }
+
+  def isSubSection(tuple: ((Int,Int),(Int,Int))): Boolean = {
+    val ((a,b),(c,d)) = tuple
+    ((a<=c && b>=d) || (c<=a && d>=b))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val filename = "src/main/scala/day04/input.txt"
+
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("Advent of Code 2022")
+      .config("spark.kryo.registrationRequired","false")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+    val textRdd = spark.sparkContext.textFile(filename)
+    val result1 = textRdd
+      .map(parseLine)
+      .map(t => if (isSubSection(t)) 1 else 0)
+      .reduce(_+_)
+
+    println("Result1 = " + result1)
+
+  }
+}
