a87ad15cee1f7536ccb7e2d4967c1f00fd766137
Leonardo Nascimento dos Santos <leonardo.nascimento.dos.santos@volvocars.com>
2022-12-04 13:45:46 +0100

    day04: part 2

--- src/main/scala/day04/Day04.scala
+++ src/main/scala/day04/Day04.scala
@@ -19,6 +19,11 @@ object main {
     ((a<=c && b>=d) || (c<=a && d>=b))
   }
 
+  def isOverlap(tuple: ((Int,Int),(Int,Int))): Boolean = {
+    val ((a,b),(c,d)) = tuple
+    (isSubSection(tuple) || (b>=c && a<c) || (a<=d && b>d) )
+  }
+
   def main(args: Array[String]): Unit = {
     val filename = "src/main/scala/day04/input.txt"
 
@@ -37,6 +42,13 @@ object main {
       .reduce(_+_)
 
     println("Result1 = " + result1)
+
+    val result2 = textRdd
+      .map(parseLine)
+      .map(t => if (isOverlap(t)) 1 else 0)
+      .reduce(_+_)
+
+    println("Result2 = " + result2)
 
   }
 }
