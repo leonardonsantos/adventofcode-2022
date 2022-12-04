706f9274f27cf06f67822a02fc8a83f8bb2bf910
Leonardo Nascimento dos Santos <leonardo.nascimento.dos.santos@volvocars.com>
2022-12-04 17:18:25 +0100

    day01: refact parse intpu with spark

--- src/main/scala/day01/Day01.scala
+++ src/main/scala/day01/Day01.scala
@@ -1,35 +1,9 @@
 package day01
 
 import org.apache.spark.sql.SparkSession
-import scala.io.Source
 
 object main {
-  // to parse the input it needs a order on reading, which is complicate to be done in spark
-  def parseInput(input: Iterator[String]): List[List[Int]] = {
-    input.foldLeft (List[List[Int]]()) {
-      (acc, x) => {
-        if (x == "") {
-          List[Int]() :: acc
-        } else {
-          if (acc.isEmpty)
-            List(List(x.toInt))
-          else
-            (x.toInt :: acc.head) :: acc.tail
-        }
-      }
-    }
-  }
-
   def main(args: Array[String]): Unit = {
-    val filename = "src/main/scala/day01/input.txt"
-    val groups: List[List[Int]] = parseInput(Source.fromFile(filename).getLines)
-
-    val result1 = groups.map(_.sum).max
-    println("Result1 = " + result1)
-
-    val result2 = groups.map(_.sum).sortBy(-_).take(3).sum
-    println("Result2 = " + result2)
-
     val spark = SparkSession
       .builder()
       .master("local[*]")
@@ -38,7 +12,24 @@ object main {
       .getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
 
-    val rddGroups = spark.sparkContext.parallelize(groups)
+    val filename = "src/main/scala/day01/input.txt"
+    val textRdd = spark.sparkContext.textFile(filename)
+    val withIndex = textRdd.zipWithIndex
+    val groupIndexes: Seq[Long] = withIndex
+      .filter{case (line, _) => line == ""}
+      .map(_._2)
+      .collect.toSeq
+    val groupIndexesBC = spark.sparkContext.broadcast(groupIndexes)
+    val rddGroups = withIndex
+      .filter{case (line, _) => line != ""}
+      .map{case (line,i) => {
+        val group: Long = groupIndexesBC.value
+          .foldLeft(0.toLong)((acc,j) => if (i>acc && i<j) acc else j)
+        (group, line.toInt)
+      }}
+      .groupByKey
+      .values
+
     val resultRdd1 = rddGroups.map(_.sum).max
     println("Result1 = " + resultRdd1)
 
