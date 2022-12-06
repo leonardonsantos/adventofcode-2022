9f8234a57a2856276f829da78713c9171a6ef850
Leonardo Nascimento dos Santos <leonardo.nascimento.dos.santos@volvocars.com>
2022-12-06 17:34:46 +0100

    day06: part 2

--- src/main/scala/day06/Day06.scala
+++ src/main/scala/day06/Day06.scala
@@ -4,19 +4,19 @@ import scala.io.Source
 import scala.collection.mutable.Queue
 
 object main {
-  def isAllDiff(buffer: Queue[Char]): Boolean = {
+  def isAllDiff(buffer: Queue[Char], diffSize: Int): Boolean = {
     // TODO: refactor
-    buffer.toSet.size == 4
+    buffer.toSet.size == diffSize
   }
 
-  def findMarker(stream: String): Long = {
+  def findMarker(stream: String, diffSize: Int): Long = {
     val buffer = Queue[Char]()
-    buffer.addAll(stream.substring(0,4))
+    buffer.addAll(stream.substring(0,diffSize))
     stream
       .zipWithIndex
-      .drop(4)
+      .drop(diffSize)
       .foldLeft((' ',0)){case (acc,(c,i)) => {
-        val result = if (isAllDiff(buffer) && acc._2==0) (c,i) else acc
+        val result = if (isAllDiff(buffer, diffSize) && acc._2==0) (c,i) else acc
         buffer.dequeue()
         buffer.enqueue(c)
         result
@@ -29,7 +29,9 @@ object main {
     // No spark 😢
 
     val stream = scala.io.Source.fromFile(filename).mkString
-    println("Result1 = " + findMarker(stream))
+    println("Result1 = " + findMarker(stream, 4))
+
+    println("Result2 = " + findMarker(stream, 14))
 
   }
 }
